3483643a6464c978c9c727b1b78f36cf0a1e9224
Sm03KeR <71275739+Sm03KeR@users.noreply.github.com>
2021-07-15 15:45:22 +0300

    Delete config.js

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RESULTS_PER_PAGE = 10;
-export const KEY = 'e7a13191-f9e7-47f0-9ef3-a173dc6db56b';
-export const MODAL_CLOSE_SEC = 2.5;
